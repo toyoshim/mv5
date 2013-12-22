30b5788b680a40df5aa10bf5a6afdd06a3aead48
Takashi Toyoshima <toyoshim@gmail.com>
2013-12-22 00:45:52 -0500

    add pseudo Promise

--- web.rb
+++ web.rb
@@ -1,5 +1,5 @@
 require 'sinatra'
 
 get '/' do
-    '<!DOCTYPE html><html><body style="margin: 0; cursor: none;"><script src="tmalib/tma.js"></script><script src="main.js"></script></body></html>'
+    '<!DOCTYPE html><html><body style="margin: 0; cursor: none;"><script src="promise.js"></script><script src="tmalib/tma.js"></script><script src="main.js"></script></body></html>'
 end
